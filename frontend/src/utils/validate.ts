f434551faf79bb2ed380d6a3b000a713ea9c2be4
Mateus Caldarte <mateuscaldarte@hotmail.com>
2022-05-08 21:21:33 -0300

    save avaliação

---
+++ frontend/src/utils/validate.ts
@@ -0,0 +1,7 @@
+export function validateEmail(email: any){
+    return String(email)
+    .toLowerCase()
+    .match(
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    );
+    }
